d6602313fca7fd4a3c97b437714c14ddc607e01a
王希宁 <xiningwang120@gmail.com>
2018-01-17 19:31:14 -0600

    play with mjackson expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,40 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(4);
-    if (res !== 16) {
-        throw new Error(`Expected 16, but got ${res}`);
-    }
+    expect(res).toBe(16).toBeA('number');
+});
+
+it('should expect some values', () => {
+    // expect(12).toNotBe(11);
+    // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+    // expect([1,2,3]).toExclude(10);
+    expect({
+        name: 'Lyn',
+        age: 25,
+        location: 'Philadelphia'
+    }).toInclude({
+        age: 25
+    });
+});
+
+it ('should verify first and last names are set', () => {
+    var user = {age: 25};
+    var strName = 'Xining Wang';
+    var res = utils.setNmae(user, strName);
+
+    expect(res)
+        .toBeA('object')
+        .toInclude({
+            firstName: "Xining",
+            lastName: "Wang"
+        })
+        .toEqual(user);
 });
